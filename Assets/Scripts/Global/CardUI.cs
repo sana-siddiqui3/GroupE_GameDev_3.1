5b1afc6222b0c21827b23e44641a05b63135d599
Istok <51259559+Icetok@users.noreply.github.com>
2025-01-05 18:06:52 +0100

    Update cardui to use sprites

--- Assets/Scripts/Global/CardUI.cs
+++ Assets/Scripts/Global/CardUI.cs
@@ -1,12 +1,49 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using TMPro;
 
-public class CardUI : MonoBehaviour
-
+public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public TextMeshProUGUI cardNameText;
+    public Image cardImage; // Reference to the Image component for the card sprite
+    public string cardName; // The name of the card
 
-    public void SetCardName(string cardName)
+    public void SetCardSprite(Sprite cardSprite)
+    {
+        if (cardImage != null)
+        {
+            cardImage.sprite = cardSprite;
+            cardImage.enabled = true; // Ensure the image is visible
+        }
+    }
+
+    public void SetCardName(string name)
+    {
+        cardName = name;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer entered card: " + cardName);
+        // Show the tooltip when hovering over the card
+        if (CardTooltip.instance != null)
+        {
+            CardTooltip.instance.ShowTooltip(cardName, Input.mousePosition);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // Hide the tooltip when the cursor leaves the card
+        if (CardTooltip.instance != null)
+        {
+            CardTooltip.instance.HideTooltip();
+        }
+    }
+
+        public TextMeshProUGUI cardNameText;
+
+    public void SetCardNameText(string cardName)
     {
         cardNameText.text = cardName;
     }
@@ -15,6 +52,6 @@ public class CardUI : MonoBehaviour
     {
         return cardNameText.text;
     }
-
 }
+
 
